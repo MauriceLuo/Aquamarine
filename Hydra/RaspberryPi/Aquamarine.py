d333e3c61273240af2e819a13c30a44d63de6586
MauriceLuo <luomiao.cn@gmail.com>
2025-05-03 20:37:14 +0800

    replace sleep by timer

--- Hydra/RaspberryPi/Aquamarine.py
+++ Hydra/RaspberryPi/Aquamarine.py
@@ -40,89 +40,95 @@ if __name__ == '__main__':
     
     lastButtonState = 0
     isAutoLevel = 0
-
+    
+    dt = 0.05
+    last_time = time.time()
     while True:
-        pygame.event.get()  # gets values from the joystick
-        # ser.write("R+\n".encode())  # sends in binary
+        if (time.time() - last_time >= dt):
+            last_time = time.time()
+            pygame.event.get()  # gets values from the joystick
+            # ser.write("R+\n".encode())  # sends in binary
 
-        controller.update()                 #Update Manipulator PWM
-        status = controller.get_status()    #Get Manipulator PWM
+            controller.update()                 #Update Manipulator PWM
+            status = controller.get_status()    #Get Manipulator PWM
 
-        joyX = round(joy.get_axis(0), 3)
-        joyY = -round(joy.get_axis(1), 3)
-        twist = apply_deadzone(round(joy.get_axis(2), 3), 0.35, 0.35)
-        slider = apply_deadzone(round(joy.get_axis(3), 3), 0.25, 0.25)
+            joyX = round(joy.get_axis(0), 3)
+            joyY = -round(joy.get_axis(1), 3)
+            twist = apply_deadzone(round(joy.get_axis(2), 3), 0.35, 0.35)
+            slider = apply_deadzone(round(joy.get_axis(3), 3), 0.25, 0.25)
 
-        theta = math.atan2(apply_deadzone(joyY, 0.25,0.25),
-                           apply_deadzone(joyX, 0.25,0.25))
-        power = math.hypot(apply_deadzone(joyX, 0.25,0.25),
-                           apply_deadzone(joyY, 0.25,0.25))
+            theta = math.atan2(apply_deadzone(joyY, 0.25,0.25),
+                            apply_deadzone(joyX, 0.25,0.25))
+            power = math.hypot(apply_deadzone(joyX, 0.25,0.25),
+                            apply_deadzone(joyY, 0.25,0.25))
 
-        sin = math.sin(theta - math.pi/4)
-        cos = math.cos(theta - math.pi/4)
-        maximum = max(abs(sin), abs(cos))
+            sin = math.sin(theta - math.pi/4)
+            cos = math.cos(theta - math.pi/4)
+            maximum = max(abs(sin), abs(cos))
 
-        assert(maximum != 0)
-        leftFront = power * (cos/maximum) + twist
-        rightFront = power * (sin/maximum) - twist
-        leftBack = power * (sin/maximum) + twist
-        rightBack = power * (cos/maximum) - twist
+            assert(maximum != 0)
+            leftFront = power * (cos/maximum) + twist
+            rightFront = power * (sin/maximum) - twist
+            leftBack = power * (sin/maximum) + twist
+            rightBack = power * (cos/maximum) - twist
 
-        if power + abs(twist) > 1:
-            assert((power + abs(twist)) != 0)
-            leftFront /= power + abs(twist)
-            rightFront /= power + abs(twist)
-            leftBack /= power + abs(twist)
-            rightBack /= power + abs(twist)
+            if power + abs(twist) > 1:
+                assert((power + abs(twist)) != 0)
+                leftFront /= power + abs(twist)
+                rightFront /= power + abs(twist)
+                leftBack /= power + abs(twist)
+                rightBack /= power + abs(twist)
 
-        leftFront = int(constrain(map_range(-leftFront, -1.00, 1.00, 1200, 1800),1200,1775)*4) #front two motors reversed
-        rightFront = int(constrain(map_range(-rightFront, -1.00, 1.00, 1200, 1800),1200,1775)*4)
-        leftBack = int(constrain(map_range(leftBack, -1.00, 1.00, 1200, 1800),1200,1775)*4)
-        rightBack = int(constrain(map_range(rightBack, -1.00, 1.00, 1200, 1800),1200,1775)*4)
+            leftFront = int(constrain(map_range(-leftFront, -1.00, 1.00, 1200, 1800),1200,1775)*4) #front two motors reversed
+            rightFront = int(constrain(map_range(-rightFront, -1.00, 1.00, 1200, 1800),1200,1775)*4)
+            leftBack = int(constrain(map_range(leftBack, -1.00, 1.00, 1200, 1800),1200,1775)*4)
+            rightBack = int(constrain(map_range(rightBack, -1.00, 1.00, 1200, 1800),1200,1775)*4)
 
-        if joy.get_button(0) > 0:
-            vertical = int(constrain(map_range(-slider, -1.00, 1.00, 1200, 1800),1200,1775)*4)
-        else:
-            vertical = 6000
-        
-        currentButtonState = joy.get_button(1)
-        if lastButtonState == 0 and currentButtonState == 1:
-            isAutoLevel = 1 - isAutoLevel
-
-        lastButtonState = currentButtonState
-        
-        #print(f"{leftFront}{rightFront}{leftBack}{rightBack}{vertical}")
-        
-        rs485.write(f"{leftFront}{rightFront}{leftBack}{rightBack}{vertical}{status['left']['rotate']}{status['left']['arm']}{status['left']['wrist']}{status['left']['manipulator']}{status['right']['rotate']}{status['right']['arm']}{status['right']['wrist']}{status['right']['manipulator']}{isAutoLevel}\n".encode())
-
-        print(f"{leftFront},{rightFront},{leftBack},{rightBack},{vertical},{status['left']['rotate']},{status['left']['arm']},{status['left']['wrist']},{status['left']['manipulator']},{status['right']['rotate']},{status['right']['arm']},{status['right']['wrist']},{status['right']['manipulator']},{isAutoLevel}")
-        
-        """
-        #while rs485.in_waiting <= 0:
-        #    time.sleep(0.01)
-        response = ""
-        try:
-            response = rs485.readline().decode("utf-8")
-            if response == "":
-                print("No response")
-                print()
+            if joy.get_button(0) > 0:
+                vertical = int(constrain(map_range(-slider, -1.00, 1.00, 1200, 1800),1200,1775)*4)
             else:
-                print(f"Decoded Values: {response}")
-                receivedOutput = [float(value) for value in response.split(',')]
-                roll = receivedOutput[0]
-                pitch = -receivedOutput[1]
-                yaw = receivedOutput[2]
-        except:
-            print("invaild packet")
-        """
+                vertical = 6000
+            
+            currentButtonState = joy.get_button(1)
+            if lastButtonState == 0 and currentButtonState == 1:
+                isAutoLevel = 1 - isAutoLevel
 
-        gui.main(roll, pitch, yaw, joy)
-        
-        #print(roll, pitch, yaw, joyX, joyY, twist, slider, leftFront/4, rightFront/4, leftBack/4, rightBack/4)
+            lastButtonState = currentButtonState
+            
+            #print(f"{leftFront}{rightFront}{leftBack}{rightBack}{vertical}")
+            
+            rs485.write(f"{leftFront}{rightFront}{leftBack}{rightBack}{vertical}{status['left']['rotate']}{status['left']['arm']}{status['left']['wrist']}{status['left']['manipulator']}{status['right']['rotate']}{status['right']['arm']}{status['right']['wrist']}{status['right']['manipulator']}{isAutoLevel}\n".encode())
 
-        #print("No respond")
-        # print("Error occured")
-        #print(f"Decoded Values: {response}")
-        time.sleep(0.05)
+            print(f"{leftFront},{rightFront},{leftBack},{rightBack},{vertical},{status['left']['rotate']},{status['left']['arm']},{status['left']['wrist']},{status['left']['manipulator']},{status['right']['rotate']},{status['right']['arm']},{status['right']['wrist']},{status['right']['manipulator']},{isAutoLevel}")
+            
+            """
+            #while rs485.in_waiting <= 0:
+            #    time.sleep(0.01)
+            response = ""
+            try:
+                response = rs485.readline().decode("utf-8")
+                if response == "":
+                    print("No response")
+                    print()
+                else:
+                    print(f"Decoded Values: {response}")
+                    receivedOutput = [float(value) for value in response.split(',')]
+                    roll = receivedOutput[0]
+                    pitch = -receivedOutput[1]
+                    yaw = receivedOutput[2]
+            except:
+                print("invaild packet")
+            """
+
+            gui.main(roll, pitch, yaw, joy)
+            
+            #print(roll, pitch, yaw, joyX, joyY, twist, slider, leftFront/4, rightFront/4, leftBack/4, rightBack/4)
+
+            #print("No respond")
+            # print("Error occured")
+            #print(f"Decoded Values: {response}")
+            #time.sleep(0.05)
+        else:
+            time.sleep(0.005)
 
 
