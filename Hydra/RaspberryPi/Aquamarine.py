cb0e426ee2814b9e79debe5ce917dcb682584eb8
MauriceLuo <luomiao.cn@gmail.com>
2025-05-03 21:32:43 +0800

    on9 things happened

--- Hydra/RaspberryPi/Aquamarine.py
+++ Hydra/RaspberryPi/Aquamarine.py
@@ -3,7 +3,8 @@ import time
 import pygame
 import math
 import Manipulator_Library as mani
-import Aquamarine_GUI as gui
+import GUI
+import joystick
 from config import *
 from utils import *
 
@@ -15,11 +16,9 @@ if __name__ == '__main__':
 
     print(pygame.joystick.get_count())
 
-    joy = pygame.joystick.Joystick(0)
-    joy.init()
-
-    print(f"{joy.get_name()} is connected")
-
+    joy = joystick.Joystick(0)
+    controller = mani.DualArmSystem(left_config, right_config, 1, [9,10])   #Manipulator initialize
+    print("Joysticks initialized")
 
     rs485 = serial.Serial('/dev/ttyAMA0', 57600, timeout=0)
     if (rs485.isOpen() == False):
@@ -27,7 +26,7 @@ if __name__ == '__main__':
         rs485.flushInput()
         rs485.flushOutput()
     rs485.flush()
-
+    
     # the Arduino is designed to reset everytime a serial connection is made over the USB interface, we must give it time to reboot.
     time.sleep(2)
 
@@ -35,9 +34,6 @@ if __name__ == '__main__':
     pitch = 0
     yaw = 0
 
-    controller = mani.DualArmSystem(left_config, right_config, 1, [9,10])   #Manipulator initialize
-    print("Manipulator initialized")
-    
     lastButtonState = 0
     isAutoLevel = 0
     
@@ -50,56 +46,20 @@ if __name__ == '__main__':
             # ser.write("R+\n".encode())  # sends in binary
 
             controller.update()                 #Update Manipulator PWM
-            status = controller.get_status()    #Get Manipulator PWM
+            mani_status = controller.get_status()    #Get Manipulator PWM
 
-            joyX = round(joy.get_axis(0), 3)
-            joyY = -round(joy.get_axis(1), 3)
-            twist = apply_deadzone(round(joy.get_axis(2), 3), 0.35, 0.35)
-            slider = apply_deadzone(round(joy.get_axis(3), 3), 0.25, 0.25)
-
-            theta = math.atan2(apply_deadzone(joyY, 0.25,0.25),
-                            apply_deadzone(joyX, 0.25,0.25))
-            power = math.hypot(apply_deadzone(joyX, 0.25,0.25),
-                            apply_deadzone(joyY, 0.25,0.25))
-
-            sin = math.sin(theta - math.pi/4)
-            cos = math.cos(theta - math.pi/4)
-            maximum = max(abs(sin), abs(cos))
-
-            assert(maximum != 0)
-            leftFront = power * (cos/maximum) + twist
-            rightFront = power * (sin/maximum) - twist
-            leftBack = power * (sin/maximum) + twist
-            rightBack = power * (cos/maximum) - twist
-
-            if power + abs(twist) > 1:
-                assert((power + abs(twist)) != 0)
-                leftFront /= power + abs(twist)
-                rightFront /= power + abs(twist)
-                leftBack /= power + abs(twist)
-                rightBack /= power + abs(twist)
-
-            leftFront = int(constrain(map_range(-leftFront, -1.00, 1.00, 1200, 1800),1200,1775)*4) #front two motors reversed
-            rightFront = int(constrain(map_range(-rightFront, -1.00, 1.00, 1200, 1800),1200,1775)*4)
-            leftBack = int(constrain(map_range(leftBack, -1.00, 1.00, 1200, 1800),1200,1775)*4)
-            rightBack = int(constrain(map_range(rightBack, -1.00, 1.00, 1200, 1800),1200,1775)*4)
-
-            if joy.get_button(0) > 0:
-                vertical = int(constrain(map_range(-slider, -1.00, 1.00, 1200, 1800),1200,1775)*4)
-            else:
-                vertical = 6000
+            joy.update()
+            thruster_status = joy.get_status()
             
-            currentButtonState = joy.get_button(1)
+            currentButtonState = thruster_status["pidButton"]
             if lastButtonState == 0 and currentButtonState == 1:
                 isAutoLevel = 1 - isAutoLevel
 
             lastButtonState = currentButtonState
             
-            #print(f"{leftFront}{rightFront}{leftBack}{rightBack}{vertical}")
-            
-            rs485.write(f"{leftFront}{rightFront}{leftBack}{rightBack}{vertical}{status['left']['rotate']}{status['left']['arm']}{status['left']['wrist']}{status['left']['manipulator']}{status['right']['rotate']}{status['right']['arm']}{status['right']['wrist']}{status['right']['manipulator']}{isAutoLevel}\n".encode())
+            rs485.write(f"{thruster_status["leftFront"]}{thruster_status["rightFront"]}{thruster_status["leftBack"]}{thruster_status["rightBack"]}{thruster_status["vertical"]}{mani_status['left']['rotate']}{mani_status['left']['arm']}{mani_status['left']['wrist']}{mani_status['left']['manipulator']}{mani_status['right']['rotate']}{mani_status['right']['arm']}{mani_status['right']['wrist']}{mani_status['right']['manipulator']}{isAutoLevel}\n".encode())
 
-            print(f"{leftFront},{rightFront},{leftBack},{rightBack},{vertical},{status['left']['rotate']},{status['left']['arm']},{status['left']['wrist']},{status['left']['manipulator']},{status['right']['rotate']},{status['right']['arm']},{status['right']['wrist']},{status['right']['manipulator']},{isAutoLevel}")
+            print(f"{thruster_status["leftFront"]},{thruster_status["rightFront"]},{thruster_status["leftBack"]},{thruster_status["rightBack"]},{thruster_status["vertical"]},{mani_status['left']['rotate']},{mani_status['left']['arm']},{mani_status['left']['wrist']},{mani_status['left']['manipulator']},{mani_status['right']['rotate']},{mani_status['right']['arm']},{mani_status['right']['wrist']},{mani_status['right']['manipulator']},{isAutoLevel}")
             
             """
             #while rs485.in_waiting <= 0:
@@ -120,9 +80,7 @@ if __name__ == '__main__':
                 print("invaild packet")
             """
 
-            gui.main(roll, pitch, yaw, joy)
-            
-            #print(roll, pitch, yaw, joyX, joyY, twist, slider, leftFront/4, rightFront/4, leftBack/4, rightBack/4)
+            GUI.main(roll, pitch, yaw, joy)
 
             #print("No respond")
             # print("Error occured")
